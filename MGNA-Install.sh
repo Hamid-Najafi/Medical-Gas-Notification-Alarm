e559748cdcd7cf4673b0320ef8cf190558c9a5b5
Hamid <Hamid.Najafi@email.com>
2023-01-06 10:46:03 +0330

    init

--- MGNA-Install.sh
+++ MGNA-Install.sh
@@ -8,9 +8,7 @@
 # Password: 1478963
 # -------==========-------
 # To Run This Script
-# wget https://raw.githubusercontent.com/Hamid-Najafi/C1-Control-Panel/main/ControlPanel-Install.sh && chmod +x ControlPanel-Install.sh && sudo ./ControlPanel-Install.sh
-# OR
-# wget https://b2n.ir/g03701 -O CP-Install.sh && chmod +x CP-Install.sh && sudo ./CP-Install.sh
+# wget https://raw.githubusercontent.com/Hamid-Najafi/Medical-Gas-Notification-Alarm/main/MGNA-Install.sh && chmod +x MGNA-Install.sh && sudo ./MGNA-Install.sh
 # -------==========-------
 echo "-------------------------------------"
 echo "Configuring User & Groups"
@@ -22,7 +20,7 @@ usermod -a -G dialout fumpict
 usermod -a -G audio fumpict
 usermod -a -G video fumpict
 usermod -a -G input fumpict
-echo "c1tech user added to sudo, dialout, audio, video & input groups"
+echo "fumpict user added to sudo, dialout, audio, video & input groups"
 # Give fumpict Reboot Permision
 chown root:fumpict /bin/systemctl
 sudo chmod 4755 /bin/systemctl
@@ -43,140 +41,75 @@ echo "-------------------------------------"
 echo "Installing Qt & Tools"
 echo "-------------------------------------"
 apt install -y mesa-common-dev libfontconfig1 libxcb-xinerama0 libglu1-mesa-dev
-apt install -y qtbase5-dev qt5-qmake libqt5quickcontrols2-5 libqt5virtualkeyboard5*  libqt5webengine5 qtmultimedia5* libqt5serial*  libqt5multimedia*   qtwebengine5-dev libqt5svg5-dev libqt5qml5 libqt5quick5  qttools5*
-apt install -y qml-module-qtquick* qml-module-qt-labs-settings qml-module-qtgraphicaleffects
-# apt install -y qtcreator
-# add-apt-repository ppa:beineri/opt-qt-5.15.4-focal
-# apt update
-# apt-get install qt515-meta-minimal -y
-# apt-get install qt515-meta-full -y
-# export LD_LIBRARY_PATH=/opt/qt515/lib/
-echo "-------------------------------------"
-echo "Configuring Music"
-echo "-------------------------------------"
-# apt install -y alsa alsa-tools alsa-utils pulseaudio portaudio19-dev libportaudio2 libportaudiocpp0
-# apt install -y libasound2-dev libpulse-dev gstreamer1.0-omx-* gstreamer1.0-alsa gstreamer1.0-plugins-good libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev  
-# apt purge -y pulseaudio
-# rm -rf /etc/pulse
-# apt install -y pulseaudio
-# amixer sset 'Master' 100%
-# amixer sset 'Capture' 85%
-# amixer sset 'Rear Mic Boost' 70%
-echo "-------------------------------------"
-echo "Configuring Vosk"
-echo "-------------------------------------"
-# string="options snd-hda-intel id=PCH,HDMI index=1,0"
-# file="/etc/modprobe.d/alsa-base.conf"
-# if ! grep -q "$string" "$file"; then
-#   echo "$string" | tee -a "$file"
-# fi
-# sudo -H -u c1tech bash -c 'pip3 install sounddevice vosk'
-# cat >> /home/c1tech/./DownloadVoskModel.py << EOF
-# from vosk import Model
-# model = Model(model_name="vosk-model-small-fa-0.5")
-# exit()
-# EOF
-# sudo -H -u c1tech bash -c 'python3 /home/c1tech/./DownloadVoskModel.py'
-# rm /home/c1tech/./DownloadVoskModel.py
-#alsamixer
-echo "-------------------------------------"
-echo "Installing PJSIP"
-echo "-------------------------------------"
-url="https://github.com/pjsip/pjproject.git"
-folder="/home/c1tech/pjproject"
-[ -d "${folder}" ] && rm -rf "${folder}"    
-git clone "${url}" "${folder}"
-cd pjproject
-./configure --prefix=/usr --enable-shared
-make dep -j4 
-make -j4
-make install
-# Update shared library links.
-ldconfig
-# Verify that pjproject has been installed in the target location
-ldconfig -p | grep pj
-cd /home/c1tech/
-echo "-------------------------------------"
-echo "Installing USB Auto Mount"
-echo "-------------------------------------"
-apt install -y liblockfile-bin liblockfile1 lockfile-progs
-url="https://github.com/rbrito/usbmount"
-folder="/home/c1tech/usbmount"
-[ -d "${folder}" ] && rm -rf "${folder}"    
-git clone "${url}" "${folder}"
-cd /home/c1tech/usbmount
-dpkg-buildpackage -us -uc -b
-cd /home/c1tech/
-dpkg -i usbmount_0.0.24_all.deb
+apt install -y qtbase5-dev qt5-qmake libqt5quickcontrols2-5 libqt5virtualkeyboard5* qtdeclarative5-dev qtmultimedia5* libqt5serial*  libqt5multimedia* libqt5svg5-dev libqt5qml5 libqt5quick5  qttools5*
+apt install -y qml-module-qtquick* qml-module-qt-labs-settings qml-module-qtgraphicaleffects 
 echo "-------------------------------------"
 echo "Installing Contold Panel Application"
 echo "-------------------------------------"
-url="https://github.com/Hamid-Najafi/C1-Control-Panel.git"
-folder="/home/c1tech/C1-Control-Panel"
-[ -d "${folder}" ] && rm -rf "${folder}"    
+url="https://github.com/Hamid-Najafi/Medical-Gas-Notification-Alarm.git"
+folder="/home/fumpict/Medical-Gas-Notification-Alarm"
+[ -d "${folder}" ] && rm -rf "${folder}"
 git clone "${url}" "${folder}"
-folder="/home/c1tech/C1"
-[ -d "${folder}" ] && rm -rf "${folder}"    
-mv /home/c1tech/C1-Control-Panel/C1 /home/c1tech/
-cd /home/c1tech/C1-Control-Panel/Panel
+folder="/home/fumpict/C1"
+[ -d "${folder}" ] && rm -rf "${folder}"
+cd /home/fumpict/Medical-Gas-Notification-Alarm/MGNA
 touch -r *.*
 qmake
 make -j4 
 
-chown -R c1tech:c1tech /home/c1tech/C1
-chown -R c1tech:c1tech /home/c1tech/C1-Control-Panel
+chown -R fumpict:fumpict /home/fumpict/Medical-Gas-Notification-Alarm
+ln -s /home/fumpict/Medical-Gas-Notification-Alarm/MGNA/passBox /home/fumpict/Medical-Gas-Notification-Alarm/MGNA/mgna
 echo "-------------------------------------"
 echo "Creating Service for Contold Panel Application"
 echo "-------------------------------------"
 journalctl --vacuum-time=60d
-loginctl enable-linger c1tech
-sudo -H -u c1tech bash << "EOF2"
-export XDG_RUNTIME_DIR=/run/user/1000
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-# /usr/lib/systemd/user/orcp.service
-# /etc/systemd/system/orcp.service
-mkdir -p /home/c1tech/.config/systemd/user
-cat > /home/c1tech/.config/systemd/user/orcp.service << "EOF"
+loginctl enable-linger fumpict
+
+cat > /etc/systemd/system/mgna.service << "EOF"
 [Unit]
-Description=C1Tech Operating Room Control Panel V2.0
-# After=pulseaudio.service
+Description=FUMP-ICT Medical Gas Notification Alarm v1.0
 
 [Service]
-# Type=idle
-Environment="XDG_RUNTIME_DIR=/run/user/1000"
-Environment="DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus"
-ExecStartPre=amixer sset 'Capture' 85% 
-ExecStart=/home/c1tech/C1-Control-Panel/Panel/panel -platform eglfs
+Type=idle
+Environment="QT_QPA_EGLFS_HIDECURSOR=1"
+ExecStart=/home/fumpict/Medical-Gas-Notification-Alarm/MGNA/mgna -platform eglfs
 Restart=always
-# User=c1tech
+User=root
 [Install]
 WantedBy=default.target
 EOF
 
-systemctl --user daemon-reload
-systemctl --user enable orcp
-EOF2
+systemctl daemon-reload
+systemctl enable mgna
 
-# systemctl --user status orcp
-# systemctl --user restart orcp
-# journalctl --user --unit orcp --follow
 echo "-------------------------------------"
 echo "Configuring Splash Screen"
 echo "-------------------------------------"
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/g' /etc/default/grub
-update-grub
+File=/boot/cmdline.txt
+String=\ quiet\ splash\ plymouth.ignore-serial-consoles
+if grep -q "$String" "$File"; then
+echo "Boot CMDLINE OK"
+else
+truncate -s-1 "$File"
+echo -n "$String" >> /boot/cmdline.txt
+fi
+
+sudo nano /boot/config.txt
+disable_splash=1
 
 apt -y autoremove --purge plymouth
 apt -y install plymouth plymouth-themes
+# plymouth-set-default-theme --list
+sudo plymouth-set-default-theme spinner
+
 # By default ubuntu-text is active 
 # /usr/share/plymouth/themes/ubuntu-text/ubuntu-text.plymouth
 # We Will use bgrt (which is same as spinner but manufacture logo is enabled) theme with our custom logo
 cp /usr/share/plymouth/themes/spinner/bgrt-fallback.png{,.bak}
 cp /usr/share/plymouth/themes/spinner/watermark.png{,.bak}
 cp /usr/share/plymouth/ubuntu-logo.png{,.bak}
-# cp /home/c1tech/C1-Control-Panel/bgrt-c1.png /usr/share/plymouth/themes/spinner/bgrt-fallback.png
-cp /home/c1tech/C1-Control-Panel/watermark-ict.png /usr/share/plymouth/themes/spinner/watermark.png
-cp /home/c1tech/C1-Control-Panel/watermark-ict.png /usr/share/plymouth/ubuntu-logo.png
+# cp /home/fumpict/Medical-Gas-Notification-Alarm/bgrt-c1.png /usr/share/plymouth/themes/spinner/bgrt-fallback.png
+cp /home/fumpict/Medical-Gas-Notification-Alarm/watermark-ict.png /usr/share/plymouth/themes/spinner/watermark.png
 update-initramfs -u
 # update-alternatives --list default.plymouth
 # update-alternatives --display default.plymouth
@@ -198,5 +131,5 @@ python3 test_microphone.py -m fa
 # -------==========-------
 sudo apt-get --purge autoremove pulseaudio
 # -------==========-------
-sudo rm /etc/systemd/system/orcp.service
+sudo rm /etc/systemd/system/mgna.service
 sudo systemctl daemon-reload

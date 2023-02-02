cdcdcb9493d12165657a79da63ab3f82fcd39085
Hamid <Hamid.Najafi@email.com>
2023-02-02 14:35:43 +0330

    init

--- MGNA/main.cpp
+++ MGNA/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 //    QTranslator translator;
 //    const QStringList uiLanguages = QLocale::system().uiLanguages();
 //    for (const QString &locale : uiLanguages) {
-//        const QString baseName = "passBox_" + QLocale(locale).name();
+//        const QString baseName = "MGNA_" + QLocale(locale).name();
 //        if (translator.load(":/i18n/" + baseName)) {
 //            app.installTranslator(&translator);
 //            break;
